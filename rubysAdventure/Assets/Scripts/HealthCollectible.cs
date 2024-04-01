54a542f845eab278cc7f43176718c510704d1d75
haefull <trinitydearion@gmail.com>
2024-03-31 22:08:58 -0400

    Update

--- rubysAdventure/Assets/Scripts/HealthCollectible.cs
+++ rubysAdventure/Assets/Scripts/HealthCollectible.cs
@@ -2,19 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// public class HealthCollectible : MonoBehaviour
+// {
+//     void OnTriggerEnter2D(Collider2D other)
+//     {
+//         RubyController controller = other.GetComponent<RubyController>();
+
+//         if (controller != null)
+//         {
+//             if(controller.health  < controller.maxHealth)
+//             {
+//                 controller.ChangeHealth(1);
+//                 Destroy(gameObject);
+//             }
+//         }
+//     }
+// }
+
 public class HealthCollectible : MonoBehaviour
 {
+    public AudioClip collectedClip;
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
         {
-            if(controller.health  < controller.maxHealth)
+            if (controller.health < controller.maxHealth)
             {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
+            	controller.ChangeHealth(1);
+            	Destroy(gameObject);
+            
+            	controller.PlaySound(collectedClip);
             }
         }
+
     }
 }
--- rubysAdventure/Assets/Scripts/RubyController.cs
+++ rubysAdventure/Assets/Scripts/RubyController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-﻿public class RubyController : MonoBehaviour
+
+public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     
     public int maxHealth = 5;
     
     public GameObject projectilePrefab;
+    
+    public AudioClip throwSound;
+    public AudioClip hitSound;
     
     public int health { get { return currentHealth; }}
     int currentHealth;
@@ -24,6 +28,8 @@ using UnityEngine;
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
     
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +37,8 @@ using UnityEngine;
         animator = GetComponent<Animator>();
         
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -62,6 +70,19 @@ using UnityEngine;
         {
             Launch();
         }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
     
     void FixedUpdate()
@@ -82,10 +103,13 @@ using UnityEngine;
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            
+            PlaySound(hitSound);
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     
     void Launch()
@@ -96,5 +120,131 @@ using UnityEngine;
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        
+        PlaySound(throwSound);
+    } 
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
+
+// public class RubyController : MonoBehaviour
+// {
+//     public GameObject projectilePrefab;
+//     public float speed = 3.0f;
+    
+//     public int maxHealth = 5;
+//     public float timeInvincible = 2.0f;
+
+//     public int health { get { return currentHealth; }}
+//     int currentHealth;
+    
+//     bool isInvincible;
+//     float invincibleTimer;
+    
+//     Rigidbody2D rigidbody2d;
+//     float horizontal;
+//     float vertical;
+    
+//     Animator animator;
+//     Vector2 lookDirection = new Vector2(1,0);
+
+//     AudioSource audioSource;
+    
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rigidbody2d = GetComponent<Rigidbody2D>();
+//         animator = GetComponent<Animator>();
+        
+//         currentHealth = maxHealth;
+        
+//         audioSource= GetComponent<AudioSource>();
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         horizontal = Input.GetAxis("Horizontal");
+//         vertical = Input.GetAxis("Vertical");
+        
+//         Vector2 move = new Vector2(horizontal, vertical);
+        
+//         if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+//         {
+//             lookDirection.Set(move.x, move.y);
+//             lookDirection.Normalize();
+//         }
+        
+//         animator.SetFloat("Look X", lookDirection.x);
+//         animator.SetFloat("Look Y", lookDirection.y);
+//         animator.SetFloat("Speed", move.magnitude);
+        
+//         if (isInvincible)
+//         {
+//             invincibleTimer -= Time.deltaTime;
+//             if (invincibleTimer < 0)
+//                 isInvincible = false;
+//         }
+
+//         if(Input.GetKeyDown(KeyCode.C))
+//         {
+//             Launch();
+//         }
+
+//         if (Input.GetKeyDown(KeyCode.X))
+// {
+//     RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+//     if (hit.collider != null)
+//     {
+//         NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+//     if (character != null)
+//     {
+//         character.DisplayDialog();
+//     }  
+//     }
+// }
+//     }
+    
+//     void FixedUpdate()
+//     {
+//         Vector2 position = rigidbody2d.position;
+//         position.x = position.x + speed * horizontal * Time.deltaTime;
+//         position.y = position.y + speed * vertical * Time.deltaTime;
+
+//         rigidbody2d.MovePosition(position);
+//     }
+
+//     public void ChangeHealth(int amount)
+//     {
+//         if (amount < 0)
+//         {
+//             animator.SetTrigger("Hit");
+//             if (isInvincible)
+//                 return;
+            
+//             isInvincible = true;
+//             invincibleTimer = timeInvincible;
+//         }
+        
+//         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+//         Debug.Log(currentHealth + "/" + maxHealth);
+//     }
+
+//         void Launch()
+//     {
+//         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+//         Projectile projectile = projectileObject.GetComponent<Projectile>();
+//         projectile.Launch(lookDirection, 300);
+
+//         animator.SetTrigger("Launch");
+//     }
+
+//     public void PlaySound(AudioClip clip)
+//     {
+//         audioSource.PlayOneShot(clip);
+//     }
+// }
